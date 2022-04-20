17c54032786cdb5ca6caa6b8a3362eb916aadd99
karenmrdn <karenmrdn47@gmail.com>
2022-04-20 16:07:43 +0300

    prettier and eslint config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  semi: false,
+  endOfLine: "auto",
+  printWidth: 110,
+  arrowParens: "avoid",
+  tabWidth: 2,
+}
