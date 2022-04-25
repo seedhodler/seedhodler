10644fe9a907974425fcea98d76a3d2bb0267cce
karenmrdn <karenmrdn47@gmail.com>
2022-04-25 10:51:51 +0300

    tabs added

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react"
 
+import GenerateIcon from "assets/icons/GenerateWithBg.svg"
+import RestoreIcon from "assets/icons/RestoreWithBg.svg"
+
 import variables from "styles/Variables.module.scss"
 import ArrowRightIcon from "assets/icons/ArrowRight.svg"
 import { Button } from "components/Button"
@@ -9,35 +12,44 @@ import { Input } from "components/Input"
 import { Calc } from "components/Calc"
 import { Switch } from "components/Switch"
 
+import classes from "./HomePage.module.scss"
+import { Tab } from "./components/Tab"
+
 type Props = {}
 
 const HomePage: React.FC<Props> = () => {
+  const [activeTabId, setActiveTabId] = useState(0)
   const [inputValue, setInputValue] = useState("Some value")
   const [checked, setChecked] = useState(false)
 
   return (
-    <div
-      style={{
-        // width: "100%",
-        height: "100%",
-        backgroundColor: "green",
-      }}
-    >
-      {/* <Button iconRight={ArrowRightIcon} style={{ marginBottom: "1rem" }}>
-        Ok! Got it
-      </Button> */}
-      {/* <Select style={{ marginBottom: "1rem" }} /> */}
-      {/* <SelectNew /> */}
-      {/* <Input
-        count={1}
-        value={inputValue}
-        onChange={setInputValue}
-        variants={["final", "fact", "fence", "forest"]}
-      /> */}
-      {/* <Calc value={2} onPlus={() => {}} onMinus={() => {}} /> */}
-      {/* <Switch checked={checked} onChange={setChecked} /> */}
-      dsad
-    </div>
+    <>
+      <div className={classes.tabsContainer}>
+        <Tab
+          title="Generate"
+          icon={GenerateIcon}
+          active={activeTabId === 0}
+          onClick={() => setActiveTabId(0)}
+        />
+        <Tab
+          title="Restore"
+          icon={RestoreIcon}
+          active={activeTabId === 1}
+          onClick={() => setActiveTabId(1)}
+        />
+      </div>
+      {activeTabId === 0 ? (
+        <div className={classes.tabContent}>
+          <div className={classes.badgeContainer}>
+            <div className={classes.badge}></div>
+            <span className={classes.title}>Phrase</span>
+            <span className={classes.bipInfo}>BIP 39</span>
+          </div>
+        </div>
+      ) : (
+        <div className={classes.tabContent}>Tab Restore Content</div>
+      )}
+    </>
   )
 }
 
