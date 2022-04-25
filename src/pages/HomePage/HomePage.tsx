5ab3c20e0d599916a3c9b6a89af92a3484e381fa
karenmrdn <karenmrdn47@gmail.com>
2022-04-25 09:10:00 +0300

    switch component added

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,41 +4,39 @@ import variables from "styles/Variables.module.scss"
 import ArrowRightIcon from "assets/icons/ArrowRight.svg"
 import { Button } from "components/Button"
 import { Select } from "components/Select"
-import { SelectNew } from "components/SelectNew"
+// import { SelectNew } from "components/SelectNew"
 import { Input } from "components/Input"
 import { Calc } from "components/Calc"
+import { Switch } from "components/Switch"
 
 type Props = {}
 
 const HomePage: React.FC<Props> = () => {
   const [inputValue, setInputValue] = useState("Some value")
+  const [checked, setChecked] = useState(false)
 
   return (
     <div
       style={{
-        minHeight: "100vh",
-        width: "500px",
-        height: "400px",
-        margin: "auto auto",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: variables.colorBg100,
+        // width: "100%",
+        height: "100%",
+        backgroundColor: "green",
       }}
     >
-      <Button iconRight={ArrowRightIcon} style={{ marginBottom: "1rem" }}>
+      {/* <Button iconRight={ArrowRightIcon} style={{ marginBottom: "1rem" }}>
         Ok! Got it
-      </Button>
-      <Select style={{ marginBottom: "1rem" }} />
+      </Button> */}
+      {/* <Select style={{ marginBottom: "1rem" }} /> */}
       {/* <SelectNew /> */}
-      <Input
+      {/* <Input
         count={1}
         value={inputValue}
         onChange={setInputValue}
         variants={["final", "fact", "fence", "forest"]}
-      />
-      <Calc value={2} onPlus={() => {}} onMinus={() => {}} />
+      /> */}
+      {/* <Calc value={2} onPlus={() => {}} onMinus={() => {}} /> */}
+      {/* <Switch checked={checked} onChange={setChecked} /> */}
+      dsad
     </div>
   )
 }
