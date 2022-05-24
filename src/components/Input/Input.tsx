7e3235b950f0e68edb38c2b65addd1e14b525bf5
karenmrdn <karen.mu@alphax.digital>
2022-05-24 09:19:10 +0300

    input dropdown list tabulation cycle

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from "react"
+import React, { useState, useEffect, Dispatch, SetStateAction } from "react"
 import CSS from "csstype"
 import * as bip39 from "bip39"
 
@@ -27,14 +27,49 @@ const Input: React.FC<Props> = ({
 }) => {
   const classNames = [classes.input, className].join(" ")
   const wordlist = isRestore ? slip39wordlist : bip39.wordlists.english
+  const [focusedItemId, setFocusedItemId] = useState(0)
+  const variants: string[] = []
+  const isOpen = value.length !== 0 && !wordlist.some(word => word === value)
+  let variantsCounter = 0
 
-  const handleChange = (newValue: string, isDropdownItemClick?: boolean) => {
+  for (let i = 0; i < wordlist.length; i++) {
+    if (variantsCounter < 5) {
+      if (wordlist[i].includes(value.toLowerCase())) {
+        variantsCounter++
+        variants.push(wordlist[i])
+      }
+    } else {
+      break
+    }
+  }
+
+  const handleChange = (newValue: string) => {
     onChange(mnemonicArr =>
       mnemonicArr.map((word, wordIndex) => (wordIndex === index ? newValue : word)),
     )
   }
 
-  let variantsCounter = 0
+  useEffect(() => {
+    const onTabPress = (e: KeyboardEvent) => {
+      if (e.key === "Tab" && isOpen) {
+        e.preventDefault()
+        setFocusedItemId(prev => (focusedItemId < variants.length - 1 ? prev + 1 : 0))
+      } else if (e.key === "Enter" && isOpen) {
+        e.preventDefault()
+        onChange(mnemonicArr =>
+          mnemonicArr.map((word, wordIndex) => (wordIndex === index ? variants[focusedItemId] : word)),
+        )
+      }
+    }
+
+    document.addEventListener("keydown", onTabPress)
+
+    return () => document.removeEventListener("keydown", onTabPress)
+  }, [focusedItemId, variants, isOpen, variants, index])
+
+  useEffect(() => {
+    setFocusedItemId(0)
+  }, [value])
 
   return (
     <div className={classes.container} style={containerStyle}>
@@ -45,24 +80,18 @@ const Input: React.FC<Props> = ({
         onChange={e => handleChange(e.target.value)}
         className={classNames}
       />
-      {value.length !== 0 && !wordlist.some(word => word === value) && (
+      {isOpen && (
         <div className={classes.dropdownList}>
-          {wordlist.map(variant => {
-            if (variantsCounter < 5 && variant.includes(value.toLowerCase())) {
-              variantsCounter++
-              return (
-                <button
-                  onClick={() => handleChange(variant, true)}
-                  key={variant}
-                  className={classes.dropdownListItem}
-                  tabIndex={0}
-                >
-                  {variant}
-                </button>
-              )
-            }
-            return null
-          })}
+          {variants.map((variant, i) => (
+            <button
+              onClick={() => handleChange(variant)}
+              key={variant}
+              className={`${classes.dropdownListItem} ${i === focusedItemId ? classes.focused : ""}`}
+              tabIndex={0}
+            >
+              {variant}
+            </button>
+          ))}
         </div>
       )}
     </div>
