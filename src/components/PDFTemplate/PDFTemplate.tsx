b5298a592fd4fd24ade49838ddca596c34a7684e
timurliftiiev-alphax <timur.li@alphax.digital>
2023-03-29 22:47:36 +0300

    new-pdf-template

--- src/components/PDFTemplate/PDFTemplate.tsx
+++ src/components/PDFTemplate/PDFTemplate.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Document, Page, Image } from "@react-pdf/renderer"
-import { headStyles } from "./styles.js"
 
-import Share20 from "assets/pdf/Share20.png"
-import Share33 from "assets/pdf/Share33.png"
-import Seedhodler12 from "assets/pdf/Seedhodler12.png"
-import Seedhodler24 from "assets/pdf/Seedhodler24.png"
+import Share33_2 from "assets/pdf/Share33-2.png"
+import Share33_1 from "assets/pdf/Share33-1.png"
+import Share20_2 from "assets/pdf/Share20-2.png"
+import Share20_1 from "assets/pdf/Share20-1.png"
+
+import Seedhodler12_2 from "assets/pdf/Seedhodler12-2.png"
+import Seedhodler24_2 from "assets/pdf/Seedhodler24-2.png"
 
 type Props = {
   selectedWordCount: number
@@ -14,14 +16,23 @@ type Props = {
 
 const PDFTemplate: React.FC<Props> = ({ selectedWordCount, sharesNumber }) => {
   const sharePages: Array<JSX.Element> = []
-  const shareTemplate = selectedWordCount === 12 ? Share20 : Share33
-  for (let index = 0; index < sharesNumber; index++) sharePages.push(<Image src={shareTemplate} />)
+  const shareTemplate = selectedWordCount === 12 ? Share20_2 : Share33_2
+  if (sharesNumber > 1) {
+    const count = (sharesNumber - 2) / 2
+    for (let index = 0; index < count; index++) sharePages.push(<Image src={shareTemplate} />)
+    if (sharesNumber % 2 === 0)
+      sharePages.push(<Image src={selectedWordCount === 12 ? Share20_1 : Share33_1} />)
+  }
   return (
     <Document>
-      <Page size="A4" key="0" style={headStyles.pdfPage}>
-        <Image src={selectedWordCount === 12 ? Seedhodler12 : Seedhodler24} />
+      <Page orientation="landscape" size="A4" key="0">
+        <Image src={selectedWordCount === 12 ? Seedhodler12_2 : Seedhodler24_2} />
       </Page>
-      <Page size="A4">{sharePages}</Page>
+      {sharesNumber > 1 && (
+        <Page orientation="landscape" size="A4">
+          {sharePages}
+        </Page>
+      )}
     </Document>
   )
 }
--- src/components/PDFTemplate/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "@react-pdf/renderer"
-
-export const headStyles = StyleSheet.create({
-  pdfPage: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-})
